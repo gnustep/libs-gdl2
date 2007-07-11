99ad68b410beda2bd8c3a6bcdcb9c5e4de0caea0
David Ayers <ayers@fsfe.org>
2007-07-11 18:11:36 +0000

    * EOControl/EOAggregateEvent.h/m: New files with stub 	implementations. 	* EOControl/GNUmakefile: Add EOAggregateEvent.h/m.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gdl2/trunk@25317 72102866-910b-0410-8b05-ffd578937521

---
+++ EOControl/EOAggregateEvent.m
@@ -0,0 +1,54 @@
+/* -*-objc-*-
+   EOEditingContext.m
+
+   Copyright (C) 2007 Free Software Foundation, Inc.
+
+   Date: July 2007
+
+   This file is part of the GNUstep Database Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, write to the Free Software Foundation,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#include <Foundation/Foundation.h>
+
+#include "EOAggregateEvent.h"
+
+@implementation EOAggregateEvent
+
+- (id)init
+{
+  if ((self = [super init]))
+    {
+      NSZone *zone = [self zone];
+      _children = [[NSArray allocWithZone: zone] init];
+      _references = [[NSMutableSet allocWithZone: zone] init];
+      _aggregateSignatureTag = 0;
+    }
+  return self;
+}
+
+- (void)addEvent:(EOEvent *)event
+{
+  _children = RETAIN([_children arrayByAddingObject: event]);
+}
+
+- (NSArray *)events
+{
+  return _children;
+}
+
+@end
